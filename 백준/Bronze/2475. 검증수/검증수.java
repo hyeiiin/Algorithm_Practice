53d25ce13d50cf1d347174166139c36e72d91417
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-08-31 00:28:29 +0900

    [Bronze V] Title: 검증수, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+
+		int N = (a * a + b * b + c * c + d * d + e * e) % 10;
+
+		System.out.println(N);
+
+	}
+
+}
