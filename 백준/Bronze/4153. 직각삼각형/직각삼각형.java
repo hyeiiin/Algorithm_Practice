467330ea8c48376e21a176678f90addff00fc27a
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-09-05 15:24:54 +0900

    [Bronze III] Title: 직각삼각형, Time: 116 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+
+			if ((a == 0) && (b == 0) && (c == 0)) {
+				break;
+			}
+
+			if ((a * a + b * b) == c * c) {
+				sb.append("right").append("\n");
+			} else if ((a * a + c * c) == b * b) {
+				sb.append("right").append("\n");
+			} else if ((c * c + b * b) == a * a) {
+				sb.append("right").append("\n");
+			} else {
+
+				sb.append("wrong").append("\n");
+			}
+
+		}
+		System.out.println(sb); 
+	}
+}
