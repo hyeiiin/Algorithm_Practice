5b1b0516c780144f473009d702ccc221ca25d851
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-10-05 12:15:20 +0900

    [Silver III] Title: N과 M (7), Time: 656 ms, Memory: 123056 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,56 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N;
+	static int M;
+	static int[] arr;
+	static int[] output;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+		output = new int[M];
+		sb = new StringBuilder();
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+
+		backTracking(0);
+		System.out.println(sb);
+
+	}
+
+	private static void backTracking(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(output[i]).append(" ");
+
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			output[depth] = arr[i];
+			backTracking(depth + 1);
+
+		}
+
+	}
+
+}
